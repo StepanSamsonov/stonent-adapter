efe2c3e5327d79979dd7aeeb0f507003616f6b63
StepanSamsonov <mynameisericlensherr@gmail.com>
2021-09-01 07:38:43 +0300

    Fix

--- services/loader/eth/eth.go
+++ services/loader/eth/eth.go
@@ -1,9 +1,9 @@
 package eth
 
 import (
+	"context"
+	"fmt"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/vladimir3322/stonent_go/tools/api"
-	"github.com/vladimir3322/stonent_go/tools/models"
 	"time"
 )
 
@@ -18,20 +18,14 @@ func GetEthClient(url string) *ethclient.Client {
 	return ethClient
 }
 
-func getLatestBlock(ethClient *ethclient.Client) *models.Block {
-	latestBlock := api.GetLatestBlock(ethClient)
-
-	if latestBlock != nil {
-		return latestBlock
-	}
-
-	time.Sleep(time.Millisecond * 100)
-	return getLatestBlock(ethClient)
-}
-
 func GetLatestBlockNumber(ethClient *ethclient.Client) uint64 {
-	latestBlock := getLatestBlock(ethClient)
-	latestBlockNumber := uint64(latestBlock.BlockNumber)
+	latestBlockNumber, err := ethClient.BlockNumber(context.Background())
+
+	if err != nil {
+		fmt.Println(fmt.Sprintf("failed to get latest block number with error: %s, retrying", err))
+		time.Sleep(time.Millisecond * 100)
+		return GetLatestBlockNumber(ethClient)
+	}
 
 	return latestBlockNumber
 }
